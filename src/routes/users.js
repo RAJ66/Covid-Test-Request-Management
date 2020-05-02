c3ba6a6aa96d6b6a33ec46332555b284835a3407
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-05-02 23:33:25 +0100

    refactor: user entity

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const user = require("../controllers/UserController.js");
 
 //Get all users
-router.get("/", user.list);
+router.get("/users", user.getAll);
 
 //Get a single user by id
-router.get("/show/:id", user.show);
+router.get("/user/:id", user.getOne);
 
 //Save user
-router.post("/save", user.save);
+router.post("/users", user.create);
 
 //Edit/Update user
-router.post("/update/:id", user.update);
+router.put("/user/:id", user.update);
 
 //Delete user
-router.post("/delete/:id", user.delete);
+router.delete("/user/:id", user.delete);
 
 module.exports = router;

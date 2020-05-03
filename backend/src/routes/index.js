ec6d1696687d5afe0e142567d879b44779a022bc
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-05-03 15:23:46 +0100

    fix: fix merge

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const usersRouter = require("./users");
 const employeesRouter = require("./employees");
 const adminRouter = require("./admin");
+const requestRouter = require("./requests");
 
 const router = express.Router();
 
 router.use("/", employeesRouter);
 router.use("/", usersRouter);
 router.use("/", adminRouter);
+router.use("/", requestRouter);
 
 module.exports = router;

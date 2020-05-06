828b4c5608cc027bb2256d339fa2df0c2b16609d
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-06 18:20:51 +0100

    feat: added new verifications on create and update

--- backend/src/controllers/RequestController.js
+++ backend/src/controllers/RequestController.js
@@ -23,8 +23,53 @@ requestController.getOne = async function (req, res) {
 requestController.create = async function (req, res) {
   try {
     const requestId = generateUniqueId();
-    const result = await Request.create({ id: requestId, ...req.body });
-    res.json({ result });
+
+    const userInfected = "Infected";
+    const userSuspect = "Suspect";
+
+    let previousUserStates = [];
+    let infectedCount = 0;
+
+    const allRequests = await Request.find();
+
+    allRequests.map((request) => {
+      if (request.userId == req.body.userId) {
+        previousUserStates.push(request.userState);
+      }
+    });
+
+    if (allRequests.length == 0) {
+      const result = await Request.create({
+        id: requestId,
+        userState: userSuspect,
+        ...req.body,
+      });
+      res.json({ result });
+    } else {
+      previousUserStates.map((result) => {
+        if (result == "Infected") {
+          infectedCount++;
+        }
+      });
+
+      if (infectedCount === 0) {
+        const result = await Request.create({
+          id: requestId,
+          userState: userSuspect,
+          ...req.body,
+        });
+
+        res.json({ result });
+      } else {
+        const result = await Request.create({
+          id: requestId,
+          userState: userInfected,
+          ...req.body,
+        });
+
+        res.json({ result });
+      }
+    }
   } catch (e) {
     res.json({});
   }
@@ -32,8 +77,35 @@ requestController.create = async function (req, res) {
 
 requestController.update = async function (req, res) {
   const { id } = req.params;
-  const result = await Request.findByIdAndUpdate(id, req.body);
-  res.json({ result });
+
+  const userInfected = "Infected";
+  const userNotInfected = "Not Infected";
+  const testCanceled = "Canceled";
+
+  await Request.findByIdAndUpdate(id, req.body);
+
+  const request = await Request.findById(id);
+
+  if (request.testResult1 == "Positive" || request.testResult2 == "Positive") {
+    await Request.findByIdAndUpdate(id, {
+      userState: userInfected,
+      testState2: testCanceled,
+      testResult2: testCanceled,
+    });
+    const updatedRequest = await Request.findById(id);
+    res.json({ updatedRequest });
+  } else if (
+    request.testResult1 == "Negative" &&
+    request.testResult2 == "Negative"
+  ) {
+    await Request.findByIdAndUpdate(id, {
+      userState: userNotInfected,
+    });
+    const updatedRequest = await Request.findById(id);
+    res.json({ updatedRequest });
+  } else {
+    res.json({ request });
+  }
 };
 
 requestController.delete = async function (req, res) {

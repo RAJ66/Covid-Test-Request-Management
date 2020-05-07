0e2087aee458db812037ebe8dfe3705549689d0d
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-05-07 19:41:50 +0100

    feat: new frontend with routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, MatSliderModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

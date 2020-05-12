f6f5b69ee696133e244eeda099bc54b940054f35
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-05-12 13:52:58 +0100

    refactor: table request to component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,27 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  list = [];
-  displayedColumns: string[] = ['position', 'name'];
-  dataSource = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ];
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get('http://localhost:3333/apiv1/requests')
-      .subscribe((res: any) => {
-        this.list = res.requestList;
-      });
-  }
+  ngOnInit(): void {}
 }

031ba5b59fde4bd6374e2fb0d9ecf32f097c2820
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-20 17:26:54 +0100

    feat: requests service added

---
+++ frontend/src/app/services/requests.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3333/apiv1/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+  withCredentials: true,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestsService {
+  constructor(public http: HttpClient) {}
+
+  getRequests(): Observable<any> {
+    return this.http.get(`${API_URL}requests`, httpOptions);
+  }
+
+  createRequest(request: Object): Observable<any> {
+    return this.http.post(`${API_URL}requests`, request, httpOptions);
+  }
+
+  getOneRequest(requestId): Observable<any> {
+    return this.http.get(`${API_URL}request/${requestId}`, httpOptions);
+  }
+
+  updateRequest(requestId, newInformation): Observable<any> {
+    return this.http.put(
+      `${API_URL}request/${requestId}`,
+      newInformation,
+      httpOptions
+    );
+  }
+
+  deleteRequest(requestId): Observable<any> {
+    return this.http.delete(`${API_URL}request/${requestId}`, httpOptions);
+  }
+}

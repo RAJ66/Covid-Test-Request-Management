212ca79bd24d3c3dbb30668fa57684ab4031da90
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-25 16:37:12 +0100

    feat: register page functioning with api

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -30,7 +30,7 @@ export class LoginPageComponent implements OnInit {
       } else if (user.role === 'Employee') {
         this.router.navigate(['table']);
       } else {
-        this.router.navigate(['/']);
+        this.router.navigate(['/home']);
       }
     });
   }

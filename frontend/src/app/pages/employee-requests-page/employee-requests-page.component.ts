e95512467dc7660dba65f227e10435030e8794a6
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-27 19:39:21 +0100

    feat: sorting in employee requests added

--- frontend/src/app/pages/employee-requests-page/employee-requests-page.component.ts
+++ frontend/src/app/pages/employee-requests-page/employee-requests-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RequestsService } from '../../services/requests.service';
 import { DataService } from '../../services/data.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-employee-requests-page',
@@ -9,6 +11,7 @@ import { DataService } from '../../services/data.service';
 })
 export class EmployeeRequestsPageComponent implements OnInit {
   list = [];
+  dataSource: any;
   requestId: string;
   displayedColumns: string[] = [
     'id',
@@ -26,11 +29,15 @@ export class EmployeeRequestsPageComponent implements OnInit {
 
   constructor(public requests: RequestsService, public data: DataService) {}
 
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   ngOnInit(): void {
     this.requests
       .getRequests(`employeeNif=${this.employeeNif}`)
       .subscribe((res: any) => {
         this.list = res.requestList;
+        this.dataSource = new MatTableDataSource(res.requestList);
+        this.dataSource.sort = this.sort;
       });
 
     this.data.currentInformation.subscribe(

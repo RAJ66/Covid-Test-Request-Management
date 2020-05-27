0cd28b17f86e8e48398f1d4117b3a47685630de6
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-27 14:30:10 +0100

    feat: added information in update request page

--- frontend/src/app/pages/update-request-page/update-request-page.component.ts
+++ frontend/src/app/pages/update-request-page/update-request-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { RequestsService } from '../../services/requests.service';
 
 @Component({
   selector: 'app-update-request-page',
   templateUrl: './update-request-page.component.html',
-  styleUrls: ['./update-request-page.component.css']
+  styleUrls: ['./update-request-page.component.css'],
 })
 export class UpdateRequestPageComponent implements OnInit {
+  information: string;
+  request: any;
+  test: string;
 
-  constructor() { }
+  constructor(public data: DataService, public requests: RequestsService) {}
 
   ngOnInit(): void {
+    this.data.currentInformation.subscribe(
+      (information) => (this.information = information)
+    );
+
+    this.requests.getOneRequest(this.information).subscribe((res) => {
+      this.request = res.request;
+    });
   }
 
+  show() {
+    console.log(this.test);
+  }
 }

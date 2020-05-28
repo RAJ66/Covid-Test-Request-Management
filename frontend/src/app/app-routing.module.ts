99576eea4006c06867bb9ccf4cc554662ccb8a18
Vitor <vitorbarbosa1998@hotmail.com>
2020-05-28 11:05:27 +0100

    refactor: clear routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { ListRequestComponent } from './list-request/list-request.component';
-import { TableRequestComponent } from './table-request/table-request.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TestMaterialComponent } from './test-material/test-material.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -16,10 +12,7 @@ import { EmployeeRequestsPageComponent } from './pages/employee-requests-page/em
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: 'list', component: ListRequestComponent },
-  { path: 'table', component: TableRequestComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'material', component: TestMaterialComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'question', component: QuestionPageComponent },

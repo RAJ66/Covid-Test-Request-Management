bbf5301841969377e0ae44416964ed30a3298d76
Vitor <vitorbarbosa1998@hotmail.com>
2020-05-28 10:39:32 +0100

    reafactor: clear code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { ListRequestComponent } from './list-request/list-request.component';
-import { TableRequestComponent } from './table-request/table-request.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TestMaterialComponent } from './test-material/test-material.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -13,10 +8,7 @@ import { UserPageComponent } from './pages/user-page/user-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: 'list', component: ListRequestComponent },
-  { path: 'table', component: TableRequestComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'material', component: TestMaterialComponent },
+
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'question', component: QuestionPageComponent },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,17 +9,14 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './components/header/header.component';
-import { RequestComponent } from './request/request.component';
 import { MatTableModule } from '@angular/material/table';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ListRequestComponent } from './list-request/list-request.component';
-import { TableRequestComponent } from './table-request/table-request.component';
-import { TestMaterialComponent } from './test-material/test-material.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
 import { KpiComponent } from './components/kpi/kpi.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+//new material
 import { DemoMaterialModule } from './material-modules';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { QuestionPageComponent } from './pages/question-page/question-page.component';
@@ -30,11 +27,8 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
   declarations: [
     AppComponent,
     HeaderComponent,
-    RequestComponent,
     DashboardComponent,
-    ListRequestComponent,
-    TableRequestComponent,
-    TestMaterialComponent,
+
     LoginPageComponent,
     KpiComponent,
     RegisterPageComponent,

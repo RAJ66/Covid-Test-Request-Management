ecf17f2bcf415eaa5fb6dd9b48df02b80bf46f9d
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-29 18:21:38 +0100

    feat: user profile page added

--- frontend/src/app/pages/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/pages/user-profile-page/user-profile-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-user-profile-page',
   templateUrl: './user-profile-page.component.html',
-  styleUrls: ['./user-profile-page.component.css']
+  styleUrls: ['./user-profile-page.component.css'],
 })
 export class UserProfilePageComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(public users: UsersService) {}
 
   ngOnInit(): void {
+    this.users
+      .getUsers(`nif=${JSON.parse(localStorage.getItem('user')).nif}`)
+      .subscribe((res) => {
+        this.user = res.userList[0];
+        console.log(this.user);
+      });
   }
-
 }

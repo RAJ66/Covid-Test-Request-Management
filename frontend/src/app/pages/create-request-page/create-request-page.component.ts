5e2876ecaaa40a463afc803ea7b53808fb93dd9a
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-30 02:20:30 +0100

    feat: success message added

--- frontend/src/app/pages/create-request-page/create-request-page.component.ts
+++ frontend/src/app/pages/create-request-page/create-request-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from '../../services/requests.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-create-request-page',
@@ -7,17 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRequestPageComponent implements OnInit {
   request: any = {};
+  user: any;
   riskGroup: string;
   riskProfession: string;
   saude24: string;
+  success: boolean;
 
-  constructor() {}
+  constructor(public requests: RequestsService, public users: UsersService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.users
+      .getUsers(`nif=${JSON.parse(localStorage.getItem('user')).nif}`)
+      .subscribe((res) => {
+        this.user = res.userList[0];
+        this.success = false;
+      });
+  }
 
   submitRequest() {
     this.request.riskGroup = String(true) == this.riskGroup;
     this.request.riskProfession = String(true) == this.riskProfession;
     this.request.saude24 = String(true) == this.saude24;
+    this.request.userId = this.user._id;
+
+    this.requests.createRequest(this.request).subscribe((res) => {
+      if (!res.err) {
+        this.success = true;
+      }
+    });
   }
 }

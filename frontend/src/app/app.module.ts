6636772bf85643a20e87d1122ff208f6bebaf467
TheNumberGuy <tiagorocha1515@gmail.com>
2020-05-30 01:33:26 +0100

    feat: update profile page added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { MatTableModule } from '@angular/material/table';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { KpiComponent } from './components/kpi/kpi.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -29,6 +29,7 @@ import { RequestPageComponent } from './pages/request-page/request-page.componen
 import { UpdateRequestPageComponent } from './pages/update-request-page/update-request-page.component';
 import { EmployeeRequestsPageComponent } from './pages/employee-requests-page/employee-requests-page.component';
 import { UserProfilePageComponent } from './pages/user-profile-page/user-profile-page.component';
+import { UpdateProfilePageComponent } from './pages/update-profile-page/update-profile-page.component';
 
 @NgModule({
   declarations: [
@@ -48,6 +49,7 @@ import { UserProfilePageComponent } from './pages/user-profile-page/user-profile
     UpdateRequestPageComponent,
     EmployeeRequestsPageComponent,
     UserProfilePageComponent,
+    UpdateProfilePageComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,6 +63,7 @@ import { UserProfilePageComponent } from './pages/user-profile-page/user-profile
     FormsModule,
     MatFormFieldModule,
     DemoMaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
